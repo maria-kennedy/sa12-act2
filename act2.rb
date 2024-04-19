9ee00544d569a6abd54b7362a3245786e8c81026
Maria Kennedy <mariakingsbury1@gmail.com>
2024-04-19 16:18:04 -0500

    p2 complete

--- act2.rb
+++ act2.rb
@@ -19,4 +19,28 @@ def prob1
     puts "URL: #{highest['html_url']}"
 end
 
-puts prob1
+# puts prob1
+
+# Problem 2
+def prob2
+    # get request
+    response = HTTParty.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
+    
+    #parsing response
+    data = JSON.parse(response.body)
+
+    #sort by market cap in descending order
+    sorted = data.sort_by { |cc| cc['market_cap'] }.reverse
+
+    # Output: List the names and current prices of the top 5 cryptocurrencies by market 
+    # capitalization, along with their respective market cap values.
+    top_5 = sorted[0..4]
+    top_5.each do |cc|
+        puts "Name: #{cc['name']}"
+        puts "Price: #{cc['current_price']}"
+        puts "Market Cap: #{cc['market_cap']}"
+        puts ""
+    end
+end
+
+puts prob2
